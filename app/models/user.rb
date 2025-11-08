e00ad9c729f232825fe4de0d7421d9cce705c174
Tomoki-Fukasawa <74344215+Tomoki-Fukasawa@users.noreply.github.com>
2025-11-08 21:59:16 +0900

    Validates&PrepareTest

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
+  validates_format_of :password, with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze, message: 'には英字と数字の両方を含めて設定してください' 
+  with_options presence: {message: 'を入力してください'} do
+    validates :nickname
+    validates :first_name_kanji
+    validates :last_name_kanji
+    validates :first_name_kana
+    validates :last_name_kana
+    validates :birth_day
+  end
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name_kanji
+    validates :last_name_kanji
+    validates :first_name_kana
+    validates :last_name_kana
+  end
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+#FactoryBot.define do
+  #factory :user do
+   # nickname              {Faker::Name.initials}
+    #email                 {Faker::Internet.email}
+    #password              {Faker::Internet.password}
+    #password_confirmation {password}
+    #first_name_kanji      {Faker::Name.initials [ぁ-んァ-ヶ一-龥々]}
+    #last_name_kanji       {Faker::Name [ぁ-んァ-ヶ一-龥々]}
+    #first_name_kana       {Faker::Name [ぁ-んァ-ヶ一-龥々]} 
+    #last_name_kana        {Faker::Name [ぁ-んァ-ヶ一-龥々]}
+    #birth_day             {1930,01,01}
+ # end
+#end
