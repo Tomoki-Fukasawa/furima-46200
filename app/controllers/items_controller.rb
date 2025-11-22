de2c8b8c2e655feb5ee570d1b95bdee289fd9059
Tomoki-Fukasawa <74344215+Tomoki-Fukasawa@users.noreply.github.com>
2025-11-22 14:23:00 +0900

    ItemsView

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:create]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    #@items = Item.all.order('created_at DESC')
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
@@ -24,6 +24,4 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name, :item_script, :category_id, :item_state_id, :deliver_pay_id, :region_id, :deliver_day_id,
                                  :price, :image).merge(user_id: current_user.id)
   end
-
-  
 end
