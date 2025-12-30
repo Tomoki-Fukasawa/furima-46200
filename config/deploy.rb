3048d3d8414e80632ef92baf755182fd20f33f22
Tomoki-Fukasawa <74344215+Tomoki-Fukasawa@users.noreply.github.com>
2025-12-30 09:31:50 +0900

    Repair_deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.20.0'
 set :application, 'furima-46200'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'Tomoki-Fukasawa/furima-46200.git'
+set :repo_url,  'git@github.com:Tomoki-Fukasawa/furima-46200.git'
 set :branch, 'main'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
